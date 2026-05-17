bf47f61c129bec3d965e7a722ed3f46103ee4578
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-05-17 10:14:50 -0300

    Actualizadas vistas y acciones

--- models/client_risk.py
+++ models/client_risk.py
@@ -69,7 +69,7 @@ class RiskReport(models.TransientModel):
             cheque_payments = self.env['account.payment'].search([
                 ('partner_id', '=', partner.id),
                 ("state", "=", "posted"),
-                ('l10n_latam_check_payment_date', '>=', report_date),
+                #('l10n_latam_check_payment_date', '>=', report_date),
                 ("journal_id.inbound_payment_method_line_ids.payment_method_id.code", "in", ["new_third_party_checks", "in_third_party_checks"]),  # Cheques no depositados
             ])
             cheques = sum(cheque_payments.mapped('amount'))
@@ -306,7 +306,7 @@ class RiskReportLine(models.TransientModel):
             payment_domain = [
                ('partner_id', '=', line.partner_id.id),
                 ("state", "=", "posted"),
-                ('l10n_latam_check_payment_date', '>=', report_date),
+                #('l10n_latam_check_payment_date', '>=', report_date),
                 ("journal_id.inbound_payment_method_line_ids.payment_method_id.code", "in", ["new_third_party_checks", "in_third_party_checks"]),
             ]
             line.payment_ids = self.env['account.payment'].search(payment_domain)
@@ -317,7 +317,7 @@ class RiskReportLine(models.TransientModel):
             'type': 'ir.actions.act_window',
             'name': f'Líneas pendientes - {self.partner_id.name}',
             'res_model': 'sale.order.line',
-            'view_mode': 'tree,form',
+            'view_mode': 'list,form',
             'domain': [('id', 'in', self.sale_order_line_ids.ids)],
             'context': {'create': False, 'edit': False},
             'target': 'current',
@@ -329,7 +329,7 @@ class RiskReportLine(models.TransientModel):
             'type': 'ir.actions.act_window',
             'name': f'Órdenes pendientes - {self.partner_id.name}',
             'res_model': 'sale.order',
-            'view_mode': 'tree,form',
+            'view_mode': 'list,form',
             'domain': [('id', 'in', self.sale_order_ids.ids)],
             'context': {'create': False, 'edit': False},
             'target': 'current',
@@ -341,7 +341,7 @@ class RiskReportLine(models.TransientModel):
             'type': 'ir.actions.act_window',
             'name': f'Movimientos de saldo - {self.partner_id.name}',
             'res_model': 'account.move.line',
-            'view_mode': 'tree,form',
+            'view_mode': 'list,form',
             'domain': [('id', 'in', self.move_line_ids.ids)],
             'context': {'create': False, 'edit': False},
             'target': 'current',
@@ -353,7 +353,7 @@ class RiskReportLine(models.TransientModel):
             'type': 'ir.actions.act_window',
             'name': f'Cheques no depositados - {self.partner_id.name}',
             'res_model': 'account.payment',
-            'view_mode': 'tree,form',
+            'view_mode': 'list,form',
             'domain': [('id', 'in', self.payment_ids.ids)],
             'context': {'create': False, 'edit': False},
             'target': 'current',
